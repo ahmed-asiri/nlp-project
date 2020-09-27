01a6d94e8c9123eb19263208bdd6d6e9fc4ce34e
Ahmed Asiri <ia7mad13@gmail.com>
2020-09-27 12:23:11 +0300

    heruko config

--- src/server/server.js
+++ src/server/server.js
@@ -34,4 +34,4 @@ app.post("/nlp", (req, res) => {
 
 const port = process.env.PORT || 5000;
 
-app.listen(3000, () => console.log(`Listening on port ${process.env.PORT}`));
+app.listen(port, () => console.log(`Listening on port ${process.env.PORT}`));
